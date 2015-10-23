32fb78766abd795431b1f29bec405cd89868ccbc
Brad Merrill <brad_merrill@hotmail.com>
2015-10-22 19:40:55 -0700

    update nuget package dependencies

--- src/build.ps1
+++ src/build.ps1
@@ -10,13 +10,21 @@ Param(
 # powershell "$(SolutionDir)build.ps1" "$(SolutionDir)." "$(ProjectPath)" "$(TargetDir)." -verbose
 
 Set-Location $solutionDir
-$nuget = Get-Path ("packages\" + (Get-ChildItem -Path packages -Recurse -Name nuget.exe))
+$nugetPath = "packages\" +  (Get-ChildItem -Path packages -Recurse -Name nuget.exe)
+if (!(Test-Path $nugetPath)) {
+  Write-Warning "could not find nuget.exe"
+  return $null
+  }
 
+$nuget = (Resolve-Path $nugetPath).Path
 $dest = 'c:\LocalNuGet'
-if (!(Test-Path $dest)) {
+Write-Host "Testing for LocalNuGet"
+if (Test-Path $dest) {
+  Write-Host "using LocalNuGet"
+  } else {
   $dest = $targetDir
-}
-
+  Write-Host "using target $dest"
+  }
 
 & "$nuget" pack "$projectPath" -Properties Configuration=$config -OutputDirectory "$dest"  -Verbosity detailed
 
